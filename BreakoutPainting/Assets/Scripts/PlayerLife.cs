5b38edfd36a16ca97e867b4044d4a5a9d98427b0
Nathan Owens <ShadowFox_43@hotmail.com>
2018-06-12 17:28:43 +1000

    Committ four. updating game files with fixes to three issues

--- BreakoutPainting/Assets/Scripts/PlayerLife.cs
+++ BreakoutPainting/Assets/Scripts/PlayerLife.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerLife : MonoBehaviour {
+public class PlayerLife : MonoBehaviour
+{
     public float time;
     public float deathupdate = 3.1f;
     public Image image;
     public float AlphaTransparancy;
     public bool playerdead;
+    public GameObject hallucinations;
+    public GameObject painting;
+    public Text introtext;
+    public bool gamestarted;
+    public float hallucinationtimer;
+    public float hallucinationlength = 5f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         image = GetComponent<Image>();
         image.color = new Color(0, 0, 0, AlphaTransparancy);
+        hallucinationtimer = Random.Range(5f, 10f);
     }
 
     // Update is called once per frame
-    void Update () {
-        if (playerdead == false)
+    void Update()
+    {
+        if (playerdead == false && gamestarted == true)
         {
             time += Time.deltaTime;
         }
@@ -27,16 +37,27 @@ public class PlayerLife : MonoBehaviour {
             deathupdate = deathupdate + 3f;
             AlphaTransparancy += 0.05f;
         }
-            image.color = new Color(0, 0, 0, AlphaTransparancy);
+        image.color = new Color(0, 0, 0, AlphaTransparancy);
         if (AlphaTransparancy >= 1f)
         {
             playerdead = true;
         }
+        if (time >= hallucinationtimer)
+        {
+            hallucinations.SetActive(true);
+            hallucinationlength = hallucinationtimer + 5f;
+            hallucinationtimer = hallucinationtimer + Random.Range(5f, 15f);
         }
+        if (time >= hallucinationlength)
+        {
+            hallucinations.SetActive(false);
+        }
+
+    }
     public void OnPlayerDeath()
     {
         AlphaTransparancy += 0.08f;
         image = GetComponent<Image>();
         image.color = new Color(0, 0, 0, AlphaTransparancy);
     }
-    }
+}
